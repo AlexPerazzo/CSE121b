7212a48c1f2ca2148a80fd20e0bff93cbad58e51
Alex Perazzo <theodoreperazzo@byui.edu>
2023-02-09 10:06:01 -0700

    Scores add properly for top section

--- Final_Project/yahtzee.js
+++ Final_Project/yahtzee.js
@@ -1,5 +1,29 @@
-function addScores() {
-  document.querySelector("#bonus").textContent = "5";
+var scoreList = document.querySelectorAll(".topScore");
+
+function addTopScores() {
+  // let sum = scoreList.reduce((partialSum, a) => partialSum + a, 0)
+
+  let sum = parseInt(document.querySelector("#aces").value);
+  let sum2 = parseInt(document.querySelector("#twos").value);
+  let sum3 = parseInt(document.querySelector("#threes").value);
+  let sum4 = parseInt(document.querySelector("#fours").value);
+  let sum5 = parseInt(document.querySelector("#fives").value);
+  let sum6 = parseInt(document.querySelector("#sixes").value);
+  total = sum + sum2 + sum3 + sum4 + sum5 + sum6;
+
+  document.querySelector("#upper-score").textContent = total;
+
+  if (total >= 63) {
+    bonus = 35;
+    document.querySelector("#bonus").textContent = bonus;
+  } else {
+    bonus = 0;
+    document.querySelector("#bonus").textContent = bonus;
+  }
+
+  document.querySelector("#upper-total").textContent = total + bonus;
 }
 
-document.querySelector("#test").addEventListener("", addScores);
+for (var i = 0; i < scoreList.length; i++) {
+  scoreList[i].addEventListener("change", addTopScores);
+}
