71b2832db557a944d83a710be2ef4b9106586b89
Alex Perazzo <theodoreperazzo@byui.edu>
2023-02-09 11:21:57 -0700

    Lower Scores now work with totals adding properly

--- Final_Project/yahtzee.js
+++ Final_Project/yahtzee.js
@@ -1,7 +1,8 @@
-var scoreList = document.querySelectorAll(".topScore");
+var topScoreList = document.querySelectorAll(".topScore");
+var botScoreList = document.querySelectorAll(".botScore");
 
 function addTopScores() {
-  // let sum = scoreList.reduce((partialSum, a) => partialSum + a, 0)
+
 
   let sum = parseInt(document.querySelector("#aces").value);
   let sum2 = parseInt(document.querySelector("#twos").value);
@@ -22,8 +23,39 @@ function addTopScores() {
   }
 
   document.querySelector("#upper-total").textContent = total + bonus;
+  document.querySelector("#upper-total2").textContent = total + bonus;
+  
+  addGrandTotal();
 }
 
-for (var i = 0; i < scoreList.length; i++) {
-  scoreList[i].addEventListener("change", addTopScores);
+function addBotScores() {
+  let sum = parseInt(document.querySelector("#threeofakind").value);
+  let sum2 = parseInt(document.querySelector("#fourofakind").value);
+  let sum3 = parseInt(document.querySelector("#full-house").value);
+  let sum4 = parseInt(document.querySelector("#sm-straight").value);
+  let sum5 = parseInt(document.querySelector("#lg-straight").value);
+  let sum6 = parseInt(document.querySelector("#yahtzee").value);
+  let sum7 = parseInt(document.querySelector("#chance").value);
+
+  total = sum + sum2 + sum3 + sum4 + sum5 + sum6 + sum7;
+
+  document.querySelector("#bottom-total").textContent = total;
+  addGrandTotal();
 }
+
+function addGrandTotal() {
+  let total1 = parseInt(document.querySelector("#bottom-total").textContent);
+  let total2 = parseInt(document.querySelector("#upper-total2").textContent);
+  grand = total1 + total2;
+  document.querySelector("#grand-total").textContent = grand;
+}
+
+for (var i = 0; i < topScoreList.length; i++) {
+  topScoreList[i].addEventListener("change", addTopScores);
+}
+
+for (var i = 0; i < botScoreList.length; i++) {
+  botScoreList[i].addEventListener("change", addBotScores, addGrandTotal);
+}
+
+// document.querySelector("#bottom-total").addEventListener("change", addGrandTotal);
